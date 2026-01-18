739152ffff0d0c13a185e128f5180e39a00578bc
Luca Knobel <knobel.luca@gmail.com>
2026-01-18 10:35:33 +0100

    created basic dashboard setup

---
+++ app/components/dashboard/Header.vue
@@ -0,0 +1,24 @@
+<!-- <script setup lang="ts">
+const localePath = useLocalePath()
+</script> -->
+
+<template>
+  <UHeader>
+    <template #left>
+      <AppLogo />
+    </template>
+    <template #right>
+      <UColorModeButton />
+      <div class="hidden lg:flex items-center gap-2">
+        <AppLocaleSelect />
+        <AppLogoutButton />
+      </div>
+    </template>
+    <template #body>
+      <div class="flex flex-col gap-2 p-4 items-center w-full">
+        <AppLogoutButton />
+        <AppLocaleSelect />
+      </div>
+    </template>
+  </UHeader>
+</template>
